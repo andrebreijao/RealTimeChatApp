ab4576bb9738dc2f65d00acc1bc34ee92c996173
andrebreijao <andrebreijao@gmail.com>
2021-11-19 21:12:14 -0300

    formatted messages (user,time, msg)

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import {formatMessage} from './utils/message.js'
 
 const app = express();
 const httpServer = createServer(app);
@@ -13,27 +14,29 @@ const io = new Server(httpServer, {
   }
 });
 
+const botName = "Chat Cord Bot";
+
 
 io.on("connection", (socket) => {
 
   //sent a message to the single client that trigged the event
-  socket.emit('banana', 'Welcome to ChatCord!');
+  socket.emit('banana', formatMessage(botName, 'Welcome to Chat Cord!'));
 
   //All clients except the one that is connecting
   //broadcast when a user join the chat 
-  socket.broadcast.emit('banana', 'A user has joined the chat');
+  socket.broadcast.emit('banana', formatMessage(botName, 'A user has joined the chat!'));
 
   //All the client in general
   //io.emit();
 
   //Runs when the user disconnect
   socket.on('disconnect', () => {
-    io.emit('banana', "A user has left the chat")
+    io.emit('banana', formatMessage(botName, 'A user has joined the chat!'))
   })
 
   //listen to chat messages from the frontend
-  socket.on('banana', msg=>{
-    io.emit('banana', msg)
+  socket.on('banana', msg => {
+    io.emit('banana', formatMessage(botName, msg))
   })
 
 
---
+++ backend/utils/message.js
@@ -0,0 +1,9 @@
+import moment from 'moment';
+
+export const formatMessage = (userName, text)=>{
+  return {
+    userName,
+    text,
+    time: moment().format('h:mm a')
+  }
+}
